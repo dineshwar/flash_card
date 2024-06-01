5d6e9dc429aaab7832fcdb536f12e4ca48063e47
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-06-01 16:23:56 +0530

    Update app

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
+import { Request, Response } from "express";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+app
+  .listen(port, () => {
+    console.log("Server running at PORT: ", port);
+  })
+  .on("error", (error: Error) => {
+    // gracefully handle error
+    throw new Error(error.message);
+  });
