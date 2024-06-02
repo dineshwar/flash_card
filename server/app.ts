a9b0231728b6842c6bb26b4317c5cd638552d006
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-06-02 16:14:54 +0530

    Update app

--- server/app.ts
+++ server/app.ts
@@ -11,6 +11,14 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello World");
 });
 
+app.get("/login", (req: Request, res: Response) => {
+  res.status(200).send("Hello World");
+});
+
+app.get("/signup", (req: Request, res: Response) => {
+  res.status(200).send("Hello World");
+});
+
 app
   .listen(port, () => {
     console.log("Server running at PORT: ", port);
