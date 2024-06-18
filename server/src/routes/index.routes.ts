e3c79cfc84869ae2b18fadff1feaf2e12ab5c9cb
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-06-18 22:51:05 +0530

    Update routes

--- server/src/routes/index.routes.ts
+++ server/src/routes/index.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import protectedRouter from "@routes/protected.routes";
 
 export default () => {
   const app = Router();
+  app.use(protectedRouter);
   return app;
 };
