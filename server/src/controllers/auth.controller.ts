9d28d966bd478424c8b6b359ae3de2c15ef8c4a2
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-08-21 23:26:40 +0530

    Work on the API

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 export class AuthController {
   async login(req: Request, res: Response) {
+    const body = req.body;
     res.status(200).send("Hello");
   }
 
